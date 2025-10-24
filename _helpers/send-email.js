4afa47de5efcbc6b03bc8957b1a42fc180d57d2b
chanlloyd12 <christlljs@gmail.com>
2025-10-25 03:33:22 +0800

    first commit

--- _helpers/send-email.js
+++ _helpers/send-email.js
@@ -4,34 +4,6 @@ const nodemailer = require('nodemailer');
 module.exports = sendEmail;
 
 async function sendEmail({ to, subject, html, from = config.emailFrom }) {
-    // 🔑 FIX: Create the SMTP options object, prioritizing environment variables
-    // This allows credentials to be securely set on Render (prod)
-    // while falling back to config.json (or empty strings) locally.
-    const smtpOptions = {
-        host: process.env.EMAIL_HOST || config.smtpOptions.host,
-        port: process.env.EMAIL_PORT || config.smtpOptions.port,
-        auth: {
-            user: process.env.EMAIL_USER || config.smtpOptions.auth.user,
-            pass: process.env.EMAIL_PASSWORD || config.smtpOptions.auth.pass
-        }
-    };
-
-    // Nodemailer typically requires explicit setting of 'secure: false' when using port 587
-    if (smtpOptions.port === 587) {
-        smtpOptions.secure = false;
-    }
-
-    const transporter = nodemailer.createTransport(smtpOptions);
+    const transporter = nodemailer.createTransport(config.smtpOptions);
     await transporter.sendMail({ from, to, subject, html });
 }
-
-
-// const config = require('../config.json');
-// const nodemailer = require('nodemailer');
-
-// module.exports = sendEmail;
-
-// async function sendEmail({ to, subject, html, from = config.emailFrom }) {
-//     const transporter = nodemailer.createTransport(config.smtpOptions);
-//     await transporter.sendMail({ from, to, subject, html });
-// }
--- _middleware/authorize.js
+++ _middleware/authorize.js
@@ -1,11 +1,5 @@
 const jwt = require('express-jwt');
-
-// Load the secret from config.json as 'configSecret'
-const { secret: configSecret } = require('../config.json');
-
-// 🔑 FIX: Correctly declare 'secret' once, prioritizing the environment variable
-const secret = process.env.JWT_SECRET || configSecret;
-
+const { secret } = require('../config.json');
 const db = require('../_helpers/db');
 
 module.exports = authorize;
@@ -39,40 +33,3 @@ function authorize(roles = []) {
         }
     ];
 }
-
-
-// const jwt = require('express-jwt');
-// const { secret } = require('../config.json');
-// const db = require('../_helpers/db');
-
-// module.exports = authorize;
-
-// function authorize(roles = []) {
-//     // roles param can be a single role string (e.g. Role.Admin or 'Admin')
-//     // or an array of roles (e.g. [Role.Admin, Role.User] or ['Admin', 'User'])
-//     if (typeof roles === 'string') {
-//         roles = [roles];
-//     }
-
-//     return [
-//         // authenticate JWT token and attach either the user or admin to the request object (req.user)
-//         jwt({ secret, algorithms: ['HS256'] }),
-
-//         // authorize based on user role
-//         async (req, res, next) => {
-//             // get user with account id from JWT token
-//             const account = await db.Account.findByPk(req.user.id);
-
-//             // account no longer exists or role not authorized
-//             if (!account || (roles.length && !roles.includes(account.role))) {
-//                 return res.status(401).json({ message: 'Unauthorized' });
-//             }
-
-//             // authentication and authorization successful
-//             req.user.role = account.role;
-//             const refreshTokens = await account.getRefreshTokens();
-//             req.user.ownsToken = token => !!refreshTokens.find(x => x.token === token);
-//             next();
-//         }
-//     ];
-// }
